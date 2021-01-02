cd112df79f5243c8e775419f846f92f6c9a422f7
Fernando Américo <63111873+fernandoamerico@users.noreply.github.com>
2021-01-02 01:12:44 -0300

    Window e Document em JS

---
+++ Cursos/JS/js/21-Windows-e-Document.js
@@ -0,0 +1,14 @@
+//Window é um dos objetos mais importantes do Js. Ele representa um conjunto de métodos e funções pre estabelecidas pelo o próprio Js.
+
+//Para ver nossos métodos dentro de nosso objeto window, basta usasr o console.log.
+console.log(window);
+
+
+//Document é uma propriedade do window e pode ser chamada como window.document ou apenas document.
+
+console.log(window.document);
+console.log(document);
+
+//Como você pode ver, a propriedade document retorna tudo o que está dentro de nosso arquivo html linkado em nosso projeto. 
+
+//Mais adiante iremos ver como aprender a manipular esses elementos.
